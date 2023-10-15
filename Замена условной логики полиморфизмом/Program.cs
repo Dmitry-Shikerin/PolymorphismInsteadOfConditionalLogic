861e5a486e333dc3232179b659b6b31159f4c715
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2023-10-15 23:07:20 +0300

    Добавьте файлы проекта.

---
+++ Замена условной логики полиморфизмом/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace Замена_условной_логики_полиморфизмом
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var orderForm = new OrderForm();
+
+            PaymentSystem paymentSystem;
+
+            PaymantSystemFactory paymantSystemFactory = new PaymantSystemFactory
+                (new QIWI("QIWI"), new WebMoney("WebMoney"), new Card("Card"));
+
+            var systemId = orderForm.ShowForm();
+
+            paymentSystem = paymantSystemFactory.Create(systemId);
+
+            paymentSystem.ShowPaymentResult();
+        }
+    }
+
+    public class OrderForm
+    {
+        public string ShowForm()
+        {
+            Console.WriteLine("Мы принимаем: QIWI, WebMoney, Card");
+
+            Console.WriteLine("Какое системой вы хотите совершить оплату?");
+            return Console.ReadLine();
+        }
+    }
+
+    class PaymantSystemFactory
+    {
+        private readonly PaymentSystem[] _paymentSystems;
+
+        public PaymantSystemFactory(params PaymentSystem[] paymentSystems)
+        {
+            if (paymentSystems == null)
+                throw new NullReferenceException(nameof(paymentSystems));
+
+            _paymentSystems = paymentSystems;
+        }
+
+        public PaymentSystem Create(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException(nameof(name));
+
+            foreach (PaymentSystem paymentSystem in _paymentSystems)
+            {
+                if (name == paymentSystem.Name)
+                {
+                    return paymentSystem;
+                }
+            }
+
+            throw new InvalidOperationException();
+        }
+    }
+
+    abstract class PaymentSystem
+    {
+        protected PaymentSystem(string name) 
+        { 
+            if(string.IsNullOrEmpty(name))
+                throw new ArgumentNullException(nameof(name));
+
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public void ShowPaymentResult()
+        {
+            ShowTransition();
+            Console.WriteLine($"Вы оплатили с помощью {Name}");
+            Console.WriteLine($"Проверка платежа через {Name}...");
+            Console.WriteLine("Оплата прошла успешно!");
+        }
+
+        protected abstract void ShowTransition(); 
+    }
+
+    class QIWI : PaymentSystem
+    {
+        public QIWI(string name) : base(name) { }
+
+        protected override void ShowTransition()
+        {
+            Console.WriteLine("Перевод на страницу QIWI...");
+        }
+    }
+
+    class WebMoney : PaymentSystem
+    {
+        public WebMoney(string name) : base(name) { }
+
+        protected override void ShowTransition()
+        {
+            Console.WriteLine("Вызов API WebMoney...");
+        }
+    }
+
+    class Card : PaymentSystem
+    {
+        public Card(string name) : base(name) { }
+
+        protected override void ShowTransition()
+        {
+            Console.WriteLine("Вызов API банка эмитера карты Card...");
+        }
+    }
+}
